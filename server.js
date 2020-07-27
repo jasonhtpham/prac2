0eb474c2d38964cbbf05a5d7ea56e418624b57d8
Jason Pham <htpham@deakin.edu.au>
2020-07-27 14:35:47 +1000

    Fix linked list implementation

--- server.js
+++ server.js
@@ -26,16 +26,87 @@ let accounts= [
 //    the desire element is found.
 
 class Node {
-    constructor (data) {
+    constructor (data, prev, next) {
         this.data = data;
-        this.next = null; 
+        this.next = next || null; 
+        this.prev = prev || null; 
     }
 } 
 
 class LinkedList {
-    constructor (head = null){
+    constructor (){
         // this.length = 0;
-        this.head = head;
+        this.head = this.tail =  null;
+    }
+
+    append(data) {
+        // check if the list is empty -> initial with a new Node
+        if (!this.tail) {
+            this.head = this.tail = new Node(data)
+        } else {
+            let oldTail = this.tail;
+            this.tail = new Node(data);
+            oldTail.next = this.tail;
+            this.tail.prev = oldTail;
+        }
+    }
+
+    prepend(data) {
+        // check if the list is empty -> initial with a new Node
+        if (!this.tail) {
+            this.head = this.tail = new Node(data)
+        } else {
+            let oldHead = this.head;
+            this.head = new Node(data);
+            oldHead.prev = this.head;
+            this.head.next = oldHead;
+        }
+    }
+
+    deleteHead() {
+        // check if the list is empty
+        if(!this.head) {
+            return null;
+        } else {
+            let removeHead = this.head;
+            if (removeHead === this.tail) {
+                this.head = this.tail = null;
+            } else {
+                this.head = this.head.next;
+                this.head.prev = null;
+            }
+            return removeHead.data;
+        }
+    }
+
+    deleteTail() {
+        // check if the list is empty
+        if(!this.tail) {
+            return null;
+        } else {
+            let removeTail = this.tail;
+            if (removeTail === this.head) {
+                this.head = this.tail = null;
+            } else {
+                this.tail = this.tail.prev;
+                this.tail.next = null;
+            }
+            return removeHead.data;
+        }
+    }
+
+    //search node by id
+    search(id) {
+        let currentNode = this.head;
+
+        while (currentNode) {
+            if(currentNode.data.id == id) {
+                return currentNode;
+            }
+            currentNode = currentNode.next
+        }
+
+        return null;
     }
 }
 
@@ -66,10 +137,15 @@ const node3 = new Node(
     }
 )
 
-node1.next = node2;
-node2.next = node3;
+// node1.next = node2;
+// node2.next = node3;
 
-let linkedListAccounts = new LinkedList(node1);
+let linkedListAccounts = new LinkedList();
+
+linkedListAccounts.append(accounts[0]);
+linkedListAccounts.append(accounts[1]);
+linkedListAccounts.append(accounts[2]);
+
 
 const addNumbers = (x, y) => {
     return x + y
@@ -102,26 +178,31 @@ app.get('/getAccount/:id', function (req, res) {
     res.send("Account Not Found");
 })
 
-app.get('/getLinkedListAccount/:id', function (req, res) {
-    if (linkedListAccounts.head == null) {
-        res.send('No account found');
-        res.end();
-    }
-    // define a var call currentNode to keep track of the LinkedList's element
-    let currentNode = linkedListAccounts.head;
+app.get('/getLinkedListAccount', function (req, res) {
+    // if (linkedListAccounts.head == null) {
+    //     res.send('No account found');
+    //     res.end();
+    // }
+    // // define a var call currentNode to keep track of the LinkedList's element
+    // let currentNode = linkedListAccounts.head;
 
-    // loop through the list until the last element
-    while (currentNode != null) {
-        if (currentNode.data.id == req.params.id) {
-            res.send(currentNode.data);
-            res.end()
-        }
-        // update the currentNode if the desired account was not found
-        currentNode = currentNode.next
-    }
+    // // loop through the list until the last element
+    // while (currentNode != null) {
+    //     if (currentNode.data.id == req.query.id) {
+    //         res.send(currentNode.data);
+    //         res.end()
+    //     }
+    //     // update the currentNode if the desired account was not found
+    //     currentNode = currentNode.next
+    // }
+
+    let result = linkedListAccounts.search(req.query.id);
 
     //response with a msg if there is no account returned
-    res.send("Account Not Found");
+    if (!result) {
+        return res.send("Account Not Found");
+    }
+    res.send(result.data)
 })
 
 // Use express.static to serves static files saved in public folder
